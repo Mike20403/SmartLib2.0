95a3368173b771b91cccc48504a3b22b6b10ee12
21120344 <ntnhan98@gmail.com>
2023-12-08 21:39:32 +0700

    Fix some bugs

---
+++ MediaPlayer/Source/Add_Playlist.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Media_Player
+{
+    /// <summary>
+    /// Interaction logic for Add_Playlist.xaml
+    /// </summary>
+    public partial class Add_Playlist : Window
+    {
+        public Playlist NewPlaylist { get; set; }
+        public Add_Playlist()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewPlaylist = new Playlist()
+            {
+                PlaylistName = new_Playlist_name.Text,
+            };
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
